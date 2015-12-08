3cf1ea3801a97b301ad5edb2986d5ac7f5d38e07
shouko <h999342@gmail.com>
2015-12-09 02:24:16 +0800

    enhance PDO exception handling

--- show.php
+++ show.php
@@ -10,6 +10,7 @@ try{
   echo json_encode(array("result" => "success", "data" => $result));
 }catch(PDOException $e){
   http_response_code(500);
+  echo json_encode(array("message" => $e->getMessage(), "line" => $e->getLine()));
   die();
 }
 ?>
